103bea8fe0fe5951471a2696d2cd5cc7e63fb33f
billapan785-max <billapan785@gmail.com>
2026-07-15 18:04:42 +0500

    Update GarageCanvas.tsx

--- src/components/GarageCanvas.tsx
+++ src/components/GarageCanvas.tsx
@@ -245,7 +245,7 @@ export default function GarageCanvas({
       <div 
         style={{
           position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1,
-          backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center'
+          backgroundImage: `url(${backgroundImage})`, backgroundSize: '100% 100%', backgroundPosition: 'center'
         }}
       />
       <div 
