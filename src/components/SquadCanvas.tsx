ba9a10b7b555b949ff7fa3030258263d3823e013
billapan785-max <billapan785@gmail.com>
2026-07-15 18:05:29 +0500

    Update SquadCanvas.tsx

--- src/components/SquadCanvas.tsx
+++ src/components/SquadCanvas.tsx
@@ -258,7 +258,7 @@ export default function SquadCanvas({
       <div
         style={{
           position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1,
-          backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center'
+          backgroundImage: `url(${backgroundImage})`, backgroundSize: '100% 100%', backgroundPosition: 'center'
         }}
       />
       <div
